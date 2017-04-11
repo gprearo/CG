047209a75a2af734fd919a5419c0187343bc9389
gprearo <guiprearo@gmail.com>
2017-04-10 21:50:55 -0300

    Alguns comentários

--- Trabalhos/T1/cg.c
+++ Trabalhos/T1/cg.c
@@ -10,13 +10,13 @@
 #define WIDTH 400 
 
 poligono p ;
-int make_resources(void) {
-	return 1 ;
-}
 
+//OpenGL Stuff
 void update_fade_factor(void) {}
 
 void render(void) {
+	//Não sei
+	//OpenGL Stuff
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f) ;
 	glClear(GL_COLOR_BUFFER_BIT) ;
 
@@ -25,6 +25,9 @@ void render(void) {
 }
 
 void mouseClick(int button, int state, int x, int y) {
+	//Se o clique for com o botão esquerdo, grava o pixelque foi clicado na
+	//estrutura de dados do poligono, imprime as coordenadas, colore o ponto
+	//que foi clicado e cria uma reta com o ponto anterior, se existente
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
 		printf("x: %d, y: %d\n", x, y) ;
 		glBegin(GL_POINTS) ;
@@ -40,10 +43,24 @@ void mouseClick(int button, int state, int x, int y) {
 		} 
 		glFlush() ;
 		addVertice(&p, x, y) ;	
+	} else {
+		//Se for clicado com o botão direito, fecha o polígono
+		if (button == GLUT_RIGHT_BUTTON && state == GLUT_UP) {
+			glBegin(GL_LINES) ;
+			glVertex2i(p.inicio->x, HEIGHT - p.inicio->y) ;
+			glVertex2i(p.fim->x, HEIGHT - p.fim->y) ;
+			glEnd() ;
+			glFlush() ;
+
+		}
 	}
 }
 
-int main(int argc, char **argv)  {
+
+//Inicializa a janela
+//OpenGL Stuff
+void initWindow(int argc, char **argv) {
+
 	glutInit(&argc, argv) ;
 	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE) ;
 	glutInitWindowSize(WIDTH, HEIGHT) ;
@@ -52,20 +69,23 @@ int main(int argc, char **argv)  {
 	glutIdleFunc(&update_fade_factor) ;
 	glMatrixMode(GL_PROJECTION);
 	glEnable(GL_BLEND) ;
-	glViewport( 0,0, HEIGHT, WIDTH);
+	glViewport( 0,0, WIDTH, HEIGHT);
 	glMatrixMode( GL_PROJECTION );
 	glLoadIdentity();
-	glOrtho( 0.0, HEIGHT, 0.0, WIDTH, 1.0, -1.0 );
+	glOrtho( 0.0, WIDTH, 0.0, HEIGHT, 1.0, -1.0 );
+	//Define a função que trata o click do mouse
+	glutMouseFunc(mouseClick);
+	glewInit() ;
+	glutMainLoop() ;
 
+}
+
+int main(int argc, char **argv)  {
+	initWindow(argc, argv) ;
 
 	initPoligono(&p) ;
 
 
-	glutMouseFunc(mouseClick);
-
-	glewInit() ;
-
-	glutMainLoop() ;
 	return 0 ;
 	
 }
